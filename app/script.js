a3a231e65457f7e0b2055c31bb1512537eb94be7
w-alt-m <166965462+w-alt-m@users.noreply.github.com>
2024-12-25 06:07:35 -0300

    Add API fetch for characters

--- app/script.js
+++ app/script.js
@@ -1,1 +1,12 @@
-console.log("Script linked");
+const apiUrl = "https://hp-api.onrender.com/api/characters";
+
+// Fetch character data
+
+fetch(apiUrl)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log("characters fetched:", data);
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
